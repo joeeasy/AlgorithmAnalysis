f11fb08f1071d4d3a76edd698ae89fd41e96d5e0
joeeasy <jehonadabokpus@gmail.com>
2018-06-12 13:04:46 +0100

    WIP

--- server/src/index.js
+++ server/src/index.js
@@ -8,9 +8,22 @@ import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackConfig from '../../webpack.config';
 // import { scrapeRoute } from './route/index';
+import session  from 'express-session';
+import expressValidator from 'express-validator';
+import flash from 'connect-flash';
 import pug from 'pug';
 import mongoose from 'mongoose';
 import Article from './models/articles';
+import cloudinary from 'cloudinary';
+import upload from 'express-fileupload';
+
+// cloudinary configuration
+cloudinary.config({ 
+  cloud_name: 'dsxfchez8', 
+  api_key: '557836499496183', 
+  api_secret: 'PKUHg16h73APhZPubaRw25mfbdE' 
+});
+
 
 const mongoDB = 'mongodb://root:test123@ds255260.mlab.com:55260/algorithmanalysis';
 // database connection
@@ -29,10 +42,29 @@ const app = express();
 const Port = process.env.PORT || 5000;
 // console.log(process.env)
 
+// Express session
+// app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}));
+
+// Express message
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
   extended: false
 }))
+
+// file upload
+app.use(upload());
 
 // // parse application/json
 app.use(bodyParser.json())
@@ -121,14 +153,24 @@ app.get('/scienceweb/pricing', (req, res) => res.render('pricing'));
 app.get('/scienceweb/institution-signup/:price', (req, res) => res.render('register'));
 app.get('/scienceweb/payment_summary', (req, res) => res.render('payment_summary'));
 app.get('/scienceweb/searchResult', (req, res) => res.render('search_result'));
-app.get('/scienceweb/search/:articleTitle', (req, res) => res.render('search_details'));
+app.get('/scienceweb/search/:articleID', (req, res) => {
+  Article.find({_id: req.params.articleID},(err, articles) => {
+    res.render('search_details', {articles})
+  })
+});
 app.get('/scienceweb/addArticle', (req, res) => res.render('addarticle'));
-app.get('/scienceweb/articles', (req, res) => res.render('articles'));
+app.get('/scienceweb/articles', (req, res) =>{
+  Article.find((err, articles) => {
+    // console.log(articles);
+    res.render('articles', {articles});
+  });
+  
+});
 
-// POST ROUTES
+// POST ROUTES creating articles
 app.post('/scienceweb/addArticle', (req, res) => {
-  console.log(req.body);
-  res.redirect('/scienceweb')
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
  
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let image = req.files.thumbnail;
@@ -174,10 +216,57 @@ app.post('/scienceweb/searchResult', (req, res) => {
   let articles = [];
   Article.find( (err, arts) => {
     arts.filter(art => {
-      console.log(art.title)
+      // console.log(art.title)
      if( art.title.includes(keyword)) {
         articles.push(art);
       }
     })
+    // console.log(articles);
+    res.render('search_result', {articles, keyword});
+  });
+});
 
+// EDIT ARTICLE
+app.get('/scienceweb/article/edit', (req, res) => {
+  let id = req.query.articleId;
+  Article.find({_id: id}, (err, articles) => {
+    res.render('editArticle', {articles})
+  })
+});
+
+// UPDATE EDITTED ARTICLES
+app.post('/scienceweb/article/update', (req, res) => {
+  // console.log(req.params.id);
+
+  let id = req.body.id;
+  let articleModified = {
+    title: req.body.articleTitle,
+    author:  req.body.authorname,
+    branch:  req.body.branch,
+    volume:  req.body.volume,
+    issues:  req.body.issues,
+    pages:  req.body.pages,
+    publishedDate:  req.body.publishedDate,
+    articleContent:  req.body.tcontent,
+    abstract:  req.body.abstract,
+    thumbnail: req.body.thumbnail,
+    paper: req.body.paper,
+    roi: req.body.roi
+  }
+  Article.findById(id, (err, article) => {
+    if (!article)
+    return next(new Error('Could not load Document'));
+    else {
+      // do your updates here
+      article = articleModified;
+      article.save(function(err) {
+        if (err)
+          console.log('error')
+        else
+          console.log('success')
+      });
+    }
+  });
+  res.redirect('/scienceweb/articles');
+})
 app.listen(Port, (req, res) => console.log('server started http://localhost:' + Port));
